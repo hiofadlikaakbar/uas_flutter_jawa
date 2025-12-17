181cfee56c3318cd495b51799ed9ca1c4f135595
hiofadlikaakbar <hioakbar1@gmail.com>
2025-12-17 22:06:49 +0700

    membuat judul splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -4,6 +4,37 @@ class SplashScreen extends StateLessWidget {
   const SplashScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Image.asset('images/logo-global.png')));
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFEFE6),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // judul/title
+              RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  ),
+                  children: [
+                    TextSpan(text: "Selamat datang di "),
+                    TextSpan(
+                      text: "LearnCode",
+                      style: TextStyle(color: Color(0xFFFF6A00)),
+                    ),
+                    TextSpan(text: "\nnow"),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
