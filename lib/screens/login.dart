791ce458e283f952071133120124b74379bc9c01
Ihza Anasrulloh <ihzaanasrulloh55@gmail.com>
2025-12-18 18:21:34 +0700

    menambahkan login ke dalam main dan menambahkan background ke dalam login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -5,6 +5,18 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF0F2027), Color(0xFF203A43), Color(0xFF2C5364)],
+          ),
+        ),
+      ),
+    );
   }
 }
