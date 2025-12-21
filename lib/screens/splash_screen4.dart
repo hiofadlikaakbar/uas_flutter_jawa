22688a7cd7c6d85b13525785a50fb91f4084f381
Ihza Anasrulloh <ihzaanasrulloh55@gmail.com>
2025-12-22 02:59:33 +0700

    mengubah menjadi gestur detektor

--- lib/screens/splash_screen4.dart
+++ lib/screens/splash_screen4.dart
@@ -6,13 +6,20 @@ class SplashScreen4 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xFF0F2027), Color(0xFF203A43)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          Navigator.pushReplacementNamed(context, '/login');
+        },
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF0F2027), Color(0xFF203A43)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
           ),
         ),
       ),
