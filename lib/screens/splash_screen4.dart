4425c57e90808e4834cfc07b6555408c71c47b40
Ihza Anasrulloh <ihzaanasrulloh55@gmail.com>
2025-12-22 02:39:30 +0700

    membuat file dan membuat body

---
+++ lib/screens/splash_screen4.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen4 extends StatelessWidget {
+  const SplashScreen4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF0F2027), Color(0xFF203A43)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+      ),
+    );
+  }
+}
