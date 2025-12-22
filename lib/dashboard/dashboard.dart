7f6aa543837d61cd0efe0ef3e2d2a77d9878a3d1
CoupDerace <turtusibae@gmail.com>
2025-12-22 13:40:50 +0700

    add container

--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -5,6 +5,27 @@ class MyDashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var size = MediaQuery.of(context).size; 
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * 0.45,
+            decoration: const BoxDecoration(
+              color: Color(0xFF0F2027),
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFF0F2027),
+                  Color(0xFF203A43),
+                  Color(0xFF2C5364),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
