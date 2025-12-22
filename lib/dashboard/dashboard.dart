2c1ebf2b81af788ea0f8a3349614accf573f287f
CoupDerace <turtusibae@gmail.com>
2025-12-22 14:11:39 +0700

    add gift dan button pada container di dashboard

--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MyDashboard extends StatelessWidget {
   const MyDashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size; 
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
         children: <Widget>[
@@ -22,8 +23,31 @@ class MyDashboard extends StatelessWidget {
                   Color(0xFF2C5364),
                 ],
               ),
+              image: DecorationImage(
+                alignment: Alignment.centerLeft,
+                image: AssetImage('images/jawa.gif'),
+              ),
             ),
-          )
+          ),
+          SafeArea(
+            child: Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 52,
+                    width: 52,
+                    decoration: BoxDecoration(
+                      color: Color(0xFF00DDF8).withOpacity(0.2),
+                      shape: BoxShape.circle,
+                    ),
+                    child: SvgPicture.asset("../../android/icons/Filter.svg"),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
