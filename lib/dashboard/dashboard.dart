22873aaa8c53462fe6235b39af3d9f949449e9d3
CoupDerace <turtusibae@gmail.com>
2025-12-24 22:33:40 +0700

    add icon disamping icon filter, jadi ada 2 icon pada pojok kanan atas

--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:uas_flutter_jawa/constants.dart';
+import 'package:uas_flutter_jawa/widget/bottom_nav_bar.dart';
+import 'package:uas_flutter_jawa/widget/categorycard.dart';
 
 class MyDashboard extends StatelessWidget {
   const MyDashboard({super.key});
@@ -8,7 +11,6 @@ class MyDashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      
       bottomNavigationBar: BottomNavBar(),
       body: Stack(
         children: <Widget>[
@@ -37,19 +39,37 @@ class MyDashboard extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: Container(
-                      alignment: Alignment.center,
-                      height: 52,
-                      width: 52,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF00DDF8).withOpacity(0.2),
-                        shape: BoxShape.circle,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        height: 52,
+                        width: 52,
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF00DDF8).withOpacity(0.2),
+                          shape: BoxShape.circle,
+                        ),
+                        child: SvgPicture.asset(
+                          "../../android/icons/Search.svg",
+                        ),
                       ),
-                      child: SvgPicture.asset("../../android/icons/Filter.svg"),
-                    ),
+                      const SizedBox(width: 12),
+                      Container(
+                        alignment: Alignment.center,
+                        height: 52,
+                        width: 52,
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF00DDF8).withOpacity(0.2),
+                          shape: BoxShape.circle,
+                        ),
+                        child: SvgPicture.asset(
+                          "../../android/icons/Filter.svg",
+                        ),
+                      ),
+                    ],
                   ),
+
                   Text(
                     "Selamat Datang \ndi \nCodeLearnJawa",
                     style: Theme.of(context).textTheme.displayMedium?.copyWith(
@@ -112,123 +132,6 @@ class MyDashboard extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class BottomNavBar extends StatelessWidget {
-  const BottomNavBar({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-      height: 80,
-      color: Colors.white,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          BottomNavItem(
-            image: '../../images/home.png',
-            title: 'Home',
-            press: () {},
-          ),
-          BottomNavItem(
-            image: '../../images/book.png',
-            title: 'All Exercise',
-            isActive: true,
-            press: () {},
-          ),
-          BottomNavItem(
-            image: '../../images/user.png',
-            title: 'Profile',
-            press: () {},
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class BottomNavItem extends StatelessWidget {
-  final String image;
-  final String title;
-  final VoidCallback press;
-  final bool isActive;
-  const BottomNavItem({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.press,
-    this.isActive = false,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: press,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Image.asset(image, width: 30, height: 30),
-          Text(
-            title,
-            style: TextStyle(color: isActive ? Colors.blue : Colors.black),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class CategoryCard extends StatelessWidget {
-  final String title;
-  final String image;
-  final VoidCallback press;
-
-  const CategoryCard({
-    super.key,
-    required this.title,
-    required this.image,
-    required this.press,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(13),
-      child: Material(
-        color: Colors.white,
-        elevation: 3,
-        child: InkWell(
-          onTap: press,
-          borderRadius: BorderRadius.circular(13),
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  image,
-                  width: 150,
-                  height: 150,
-                  fit: BoxFit.contain,
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  title,
-                  textAlign: TextAlign.center,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              ],
-            ),
-          ),
-        ),
       ),
     );
   }
