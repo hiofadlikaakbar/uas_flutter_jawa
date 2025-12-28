d5fc096fdd55c10c2a53c6f564d86f842fbfe9ee
hiofadlikaakbar <hioakbar1@gmail.com>
2025-12-28 21:56:49 +0700

    nambah route signup di main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:uas_flutter_jawa/dashboard/dashboard.dart';
 import 'package:uas_flutter_jawa/screens/login.dart';
+import 'package:uas_flutter_jawa/screens/signup.dart';
 import 'package:uas_flutter_jawa/screens/splash_screen3.dart';
 import 'package:uas_flutter_jawa/screens/splash_screen4.dart';
 import 'screens/splash_screen.dart';
 import 'screens/splash_screen2.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -19,12 +19,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Mulai Belajar",
       initialRoute: '/',
-      routes: {'/': (context) => const SplashScreen(),
-      '/splash2': (context) => const SplashScreen2(),
-      '/splash3': (context) => const SplashScreen3(),
-      '/splash4': (context) => const SplashScreen4(),
-      '/login': (context) => const LoginPage(),
-      '/dashboard': (context) => const MyDashboard(),
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/splash2': (context) => const SplashScreen2(),
+        '/splash3': (context) => const SplashScreen3(),
+        '/splash4': (context) => const SplashScreen4(),
+        '/login': (context) => const LoginPage(),
+        '/signup': (context) => const SignupPage(),
+        '/dashboard': (context) => const MyDashboard(),
       },
     );
   }
