d2c856a5a4095db1db33e257a4e01c9e95169d91
hiofadlikaakbar <hioakbar1@gmail.com>
2025-12-28 21:39:05 +0700

    membuat file  signup

---
+++ lib/screens/signup.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupState();
+}
