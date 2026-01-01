cf283c115552bf456415da4c6ff3e579ca21370b
CoupDerace <turtusibae@gmail.com>
2026-01-01 21:29:21 +0700

    add icon back sama text profile

--- lib/screens/tur_punya.dart
+++ lib/screens/tur_punya.dart
@@ -7,7 +7,33 @@ class Tur extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(child: Column(children: const [])),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 12,
+                ),
+                child: Row(
+                  children: const [
+                    Icon(Icons.arrow_back),
+                    SizedBox(width: 8),
+                    Text(
+                      "Profile",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
