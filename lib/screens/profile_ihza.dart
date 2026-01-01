9865dadb6928ab55c0396dbb3f36c2defd70544e
Ihza Anasrulloh <ihzaanasrulloh55@gmail.com>
2026-01-01 16:10:17 +0700

    menambahkan text NAMA

--- lib/screens/profile_ihza.dart
+++ lib/screens/profile_ihza.dart
@@ -5,6 +5,25 @@ class ProfileIhza extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFF0B1623),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 120,
+            left: 20,
+            child: Text(
+              "IHZA\nANASRULLOH",
+              style: TextStyle(
+                fontSize: 72,
+                height: 0.9,
+                fontWeight: FontWeight.w900,
+                color: Colors.white.withOpacity(0.08),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
