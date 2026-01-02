4085b9743ef336e1dc342ce3f2b34b45cccd62fe
CoupDerace <turtusibae@gmail.com>
2026-01-02 10:58:34 +0700

    bikin ulang, add image

--- lib/screens/tur_punya.dart
+++ lib/screens/tur_punya.dart
@@ -5,102 +5,17 @@ class Tur extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 12,
-                ),
-                child: Row(
-                  children: const [
-                    Icon(Icons.arrow_back),
-                    SizedBox(width: 8),
-                    Text(
-                      "Profile",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const CircleAvatar(
-                radius: 60,
-                backgroundImage: AssetImage('images/Tur.jpg'),
-              ),
-
-              const SizedBox(height: 16),
-              Container(
-                margin: const EdgeInsets.all(16),
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(24),
-                  gradient: LinearGradient(
-                    colors: [Colors.purple.shade200, Colors.purple.shade50],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
-                ),
-                child: Column(
-                  children: [
-                    const Text(
-                      "Muhammad Turtusi Afrizal Perdana",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 4),
-                    const Text(
-                      "123200045",
-                      style: TextStyle(fontSize: 16, color: Colors.black54),
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: const [
-                        W1(title: "Following", value: "542"),
-                        W1(title: "Followers", value: "98K"),
-                        W1(title: "Likes", value: "100K"),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
+      body: Stack(
+        children: [
+          SizedBox(
+            height: size.height * 0.4,
+            width: size.width,
+            child: Image.asset('images/Tur.jpg', fit: BoxFit.cover),
           ),
-        ),
+        ],
       ),
-    );
-  }
-}
-
-class W1 extends StatelessWidget {
-  final String title;
-  final String value;
-
-  const W1({super.key, required this.title, required this.value});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          value,
-          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: 4),
-        Text(
-          title,
-          style: const TextStyle(fontSize: 16, color: Colors.black54),
-        ),
-      ],
     );
   }
 }
