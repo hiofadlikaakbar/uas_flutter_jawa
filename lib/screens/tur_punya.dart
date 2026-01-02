b3e06e272a537f494efe4109475b7dbd8d04b65a
CoupDerace <turtusibae@gmail.com>
2026-01-02 11:20:22 +0700

    add bottom card dengan ukuran 1/3 layar

--- lib/screens/tur_punya.dart
+++ lib/screens/tur_punya.dart
@@ -21,6 +21,58 @@ class Tur extends StatelessWidget {
                 children: const [
                   Icon(Icons.arrow_back, color: Colors.black),
                   SizedBox(width: 8),
+                  Text(
+                    "Profil Tim",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            height: size.height * 0.3,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(32),
+                  topRight: Radius.circular(32),
+                ),
+                gradient: LinearGradient(
+                  colors: [Color(0xFF0B1623), Color.fromARGB(255, 4, 56, 77)],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+              ),
+              child: Column(
+                children: const [
+                  Text(
+                    "Muhammad Turtusi Afrizal Perdana",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "JAWA 3",
+                    style: TextStyle(fontSize: 18, color: Colors.grey),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    "tusi",
+                    style: TextStyle(fontSize: 16, color: Colors.black87),
+                  ),
                 ],
               ),
             ),
