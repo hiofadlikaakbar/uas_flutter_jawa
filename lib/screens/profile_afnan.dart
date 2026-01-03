e2e7a3fe08ab571b4a661f65fd7817d341e0e2e8
afnan923 <susumurni013@gmail.com>
2026-01-03 14:40:17 +0700

    update

--- lib/screens/profile_afnan.dart
+++ lib/screens/profile_afnan.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-class ProfileAfnan extends StatefulWidget {
-  const ProfileAfnan({super.key});
+class MyAfnan extends StatelessWidget {
+  const MyAfnan({super.key});
 
-  @override
-  State<ProfileAfnan> createState() => _ProfileAfnanState();
-}
-
-class _ProfileAfnanState extends State<ProfileAfnan> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
