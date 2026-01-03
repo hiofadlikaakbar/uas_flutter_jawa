d7e1307ea3d405a25791cba32af4b1ae093fbd66
CoupDerace <turtusibae@gmail.com>
2026-01-03 10:43:15 +0700

    add widget custom

--- lib/screens/final_tur_punya.dart
+++ lib/screens/final_tur_punya.dart
@@ -71,6 +71,25 @@ class LayoutFinalPage extends StatelessWidget {
                           ),
                         ],
                       ),
+                      const SizedBox(height: 24),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          W1(title: "Followers", value: "150K"),
+                          W1(title: "Following", value: "200"),
+                          W1(title: "Projects", value: "10"),
+                        ],
+                      ),
+                      const SizedBox(height: 24),
+                      const Text(
+                        "About Me",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontFamily: 'RobotoMono',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -79,6 +98,35 @@ class LayoutFinalPage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class W1 extends StatelessWidget {
+  final String title;
+  final String value;
+
+  const W1({required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          value,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontFamily: 'Poppins',
+          ),
+        ),
+        const SizedBox(height: 4),
+        Text(
+          title,
+          style: const TextStyle(color: Colors.white70, fontFamily: 'Poppins'),
+        ),
+      ],
     );
   }
 }
