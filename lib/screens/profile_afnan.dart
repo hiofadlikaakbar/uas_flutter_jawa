62fe49a778e0d8323e58feef41a255264ade130a
afnan923 <susumurni013@gmail.com>
2026-01-03 15:01:51 +0700

    tambah fonts dan nama

--- lib/screens/profile_afnan.dart
+++ lib/screens/profile_afnan.dart
@@ -5,6 +5,28 @@ class MyAfnan extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Container(
+          width: 300,
+          padding: const EdgeInsets.symmetric(vertical: 24),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text("Afnan Dani",
+              textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontFamily: "Outfit"
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
