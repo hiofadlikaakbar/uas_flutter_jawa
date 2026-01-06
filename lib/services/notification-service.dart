633111d7aac08ffb72ef3a56de2ffd624e4b9756
afnan923 <susumurni013@gmail.com>
2026-01-06 23:23:51 +0700

    inisialisasi local notif dengan permission

--- lib/services/notification-service.dart
+++ lib/services/notification-service.dart
@@ -1,6 +1,25 @@
-
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationService {
-  static final FlutterLocalNotificationsPlugin  _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-}
+  static final FlutterLocalNotificationsPlugin
+    _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  static Future<void> init() async {
+    const AndroidInitializationSettings androidSettings =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    const InitializationSettings settings =
+        InitializationSettings(android: androidSettings);
+
+    await _flutterLocalNotificationsPlugin.initialize(settings);
+
+    // 🔴 WAJIB UNTUK ANDROID 13+
+    await _flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.requestNotificationsPermission();
+  }
+  }
+
+  
+
