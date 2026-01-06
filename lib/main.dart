ae73ec3ddfc4758db676daa1748334d4b03e6d55
afnan923 <susumurni013@gmail.com>
2026-01-06 22:04:18 +0700

    inisialisasi firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:uas_flutter_jawa/dashboard/dahboard_profile.dart';
 import 'package:uas_flutter_jawa/dashboard/dashboard.dart';
+import 'package:uas_flutter_jawa/firebase_options.dart';
 import 'package:uas_flutter_jawa/screens/login.dart';
 import 'package:uas_flutter_jawa/screens/profile_afnan.dart';
 import 'package:uas_flutter_jawa/screens/profile_ihza.dart';
@@ -15,6 +17,7 @@ import 'screens/splash_screen2.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await Supabase.initialize(
     url: 'https://qepadldcyhysdbwyxydk.supabase.co',
