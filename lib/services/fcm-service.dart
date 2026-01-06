6669c4e99ab7caec2fa40d3ab4bdbda5482ce94e
afnan923 <susumurni013@gmail.com>
2026-01-06 22:15:39 +0700

    inisialisasi flutter notif

--- lib/services/fcm-service.dart
+++ lib/services/fcm-service.dart
@@ -1,5 +1,18 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'default_channel',
+  'Default Notification',
+  description: 'Channel untuk FCM',
+  importance: Importance.high,
+);
+
 
 Future<void> setupFcm() async{
   final messaging = FirebaseMessaging.instance;
+  
 }
